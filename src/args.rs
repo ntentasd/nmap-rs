adc260033e2b88c7d89d1b4bb55d3777aeeeb157
dimitris.ntentas <dimitrisntentas2002@gmail.com>
2026-04-27 23:08:37 +0300

    feat: add multi-port scanning with range and list syntax

---
+++ src/args.rs
@@ -0,0 +1,64 @@
+use std::{num::ParseIntError, str::FromStr};
+
+use clap::Parser;
+
+/// Simple program to scan ports
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Address of the port to scan
+    #[arg(short, long, default_value_t = String::from("127.0.0.1"))]
+    pub address: String,
+
+    /// Ports to scan
+    #[arg(short, long)]
+    pub ports: Ports,
+
+    /// Number of tries
+    #[arg(short, long, default_value_t = 1)]
+    pub tries: u8,
+
+    /// Timeout duration in milliseconds
+    #[arg(long, default_value_t = 3)]
+    pub timeout: u64,
+}
+
+#[derive(Clone, Debug)]
+pub struct Ports(Vec<u16>);
+
+impl FromStr for Ports {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.contains('-') {
+            let mut split = s.split('-');
+            let (Some(start), Some(end), None) = (split.next(), split.next(), split.next()) else {
+                return Err("invalid range format".to_string());
+            };
+            let start: u16 = start.parse().map_err(|e: ParseIntError| e.to_string())?;
+            let end: u16 = end.parse().map_err(|e: ParseIntError| e.to_string())?;
+
+            if start > end {
+                return Err("invalid range".to_string());
+            }
+
+            Ok(Ports((start..=end).collect()))
+        } else {
+            let ports = s
+                .split(',')
+                .map(|s| s.parse::<u16>())
+                .collect::<Result<Vec<u16>, _>>()
+                .map_err(|e| e.to_string())?;
+
+            Ok(Ports(ports))
+        }
+    }
+}
+
+impl IntoIterator for Ports {
+    type Item = u16;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,24 @@
+mod args;
 mod report;
 mod scanner;
 
+use args::Args;
 use clap::Parser;
 use scanner::Scanner;
 use std::io::Result;
 use tokio::time::Duration;
 
-/// Simple program to scan ports
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Address of the port to scan
-    #[arg(short, long, default_value_t = String::from("127.0.0.1"))]
-    address: String,
-
-    /// Port to scan
-    #[arg(short, long)]
-    port: u16,
-
-    /// Number of tries
-    #[arg(short, long, default_value_t = 1)]
-    tries: u8,
-
-    /// Timeout duration in milliseconds
-    #[arg(long, default_value_t = 3)]
-    timeout: u64,
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
 
-    let sc = Scanner::new(format!("{}:{}", args.address, args.port), args.tries).await?;
+    for port in args.ports {
+        let sc = Scanner::new(format!("{}:{}", args.address, port), args.tries).await?;
 
-    let res = sc.scan(Duration::from_millis(args.timeout)).await?;
+        let res = sc.scan(Duration::from_millis(args.timeout)).await?;
 
-    println!("{}", res);
+        println!("{}", res);
+    }
 
     Ok(())
 }
