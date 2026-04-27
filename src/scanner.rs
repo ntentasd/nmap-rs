c454136f0c5e6caf895c8768afc3ed7585f3845b
dimitris.ntentas <dimitrisntentas2002@gmail.com>
2026-04-27 21:13:45 +0300

    feat(scanner): implement retry logic and unit tests

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,7 +1,11 @@
-use std::{io::Result, net::SocketAddr};
+use std::{
+    io::{Error, ErrorKind, Result},
+    net::SocketAddr,
+};
 
-use tokio::net::ToSocketAddrs;
+use tokio::net::{TcpStream, ToSocketAddrs};
 
+#[derive(Debug, PartialEq)]
 pub struct Scanner {
     address: SocketAddr,
     tries: u8,
@@ -9,6 +13,10 @@ pub struct Scanner {
 
 impl Scanner {
     pub async fn new(address: impl ToSocketAddrs, tries: u8) -> Result<Self> {
+        if tries == 0 {
+            return Err(Error::new(ErrorKind::InvalidInput, "invalid tries input"));
+        }
+
         let address = tokio::net::lookup_host(address)
             .await?
             .next()
@@ -22,8 +30,54 @@ impl Scanner {
     }
 
     pub async fn scan(&self) -> Result<()> {
-        let _stream = tokio::net::TcpStream::connect(self.address).await?;
-        let _ = self.tries;
-        Ok(())
+        let mut last_err: Option<Error> = None;
+        for _ in 0..self.tries {
+            match TcpStream::connect(self.address).await {
+                Ok(_stream) => return Ok(()),
+                Err(e) => {
+                    last_err = Some(e);
+                }
+            }
+        }
+        Err(last_err.unwrap())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_valid_new() {
+        let result = Scanner::new("127.0.0.1:31234", 3).await;
+        assert!(result.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_invalid_new() {
+        let result = Scanner::new("127.0.0.1:31234", 0).await;
+        assert_eq!(result.unwrap_err().kind(), ErrorKind::InvalidInput);
+    }
+
+    #[tokio::test]
+    async fn test_port_scan() {
+        let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+
+        tokio::spawn(async move {
+            let listener = tokio::net::TcpListener::bind("127.0.0.1:31234")
+                .await
+                .unwrap();
+            tx.send(()).unwrap();
+            while let Ok((_, _)) = listener.accept().await {}
+        });
+
+        rx.await.unwrap();
+
+        let result = Scanner::new("127.0.0.1:31234", 3)
+            .await
+            .unwrap()
+            .scan()
+            .await;
+        assert!(result.is_ok());
     }
 }
